16908329a5f94262bdb226c3e83cd85dfe1d1a5a
Julio Santos <nameteste08@gmail.com>
2022-01-21 10:52:45 -0300

    refatoracao de codigo

--- features/pages/login_page.rb
+++ features/pages/login_page.rb
@@ -7,3 +7,32 @@ class Login_page < SitePrism::Page
   element :credential_error, "div[class='message-body']"
   element :logged_area, :xpath, "//div[2]/h2"
 end
+
+module Actions
+  def access_page_login
+    @login.load
+    @login.btn_login.click
+  end
+
+  def fill_email_passwd(email, passwd)
+    @login.email.set(email)
+    @login.password.set(passwd)
+    @login.log_in.click
+  end
+
+  def msg_return
+    result = false
+
+    begin
+      result = @login.credential_error.visible?
+    rescue => exception
+      puts "element not found"
+    end
+
+    if result
+      @login.credential_error
+    else
+      @login.logged_area
+    end
+  end
+end
