49c23184da802fec7847106a6b8750cd1fd8b78e
Julio Cesar <68651027+julio958214@users.noreply.github.com>
2022-04-21 21:42:29 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,17 +16,23 @@ require_relative "../pages/login_page.rb"
 World(Actions)
 World(Helpers)
 
-Capybara.register_driver :selenium do |driver|
-  Capybara::Selenium::Driver.new(driver, :browser => :chrome)
+Capybara.register_driver :chrome_custom do |app|
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    clear_session_storage: true,
+    clear_local_storage: true,
+    capabilities: [Selenium::WebDriver::Chrome::Options.new(
+      args: ["--headless", "--disable-site-isolation-trials", "--disable-gpu"],
+      "excludeSwitches" => ["enable-logging"],
+    )]
 end
 
-Capybara.configure do
+Capybara.configure do |config|
   include RSpec::Matchers
-  Capybara.run_server = false
-  Capybara.default_driver = :selenium_chrome_headless
-  Capybara.page.driver.browser.manage.window.maximize
-  Capybara.default_max_wait_time = 10
-  Capybara.app_host = "https://parodify.herokuapp.com"
+  config.run_server = false
+  config.default_driver = :chrome_custom
+  config.default_max_wait_time = 20
+  config.app_host = "https://parodify.herokuapp.com"
 end
 
 AllureCucumber.configure do |config|
